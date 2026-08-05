992a4f5221c5426304a198a62e02554ca30826c8
nikvn89 <nikvn89@gmail.com>
2026-08-05 15:41:27 +0700

    fix: remove marketIds from useEffect dependency array to prevent infinite loops

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,7 @@ function App() {
   useEffect(() => {
     fetchAllMarkets();
     fetchBalance();
-  }, [marketIds, activeWallet]);
+  }, [activeWallet]);
 
   const fetchBalance = async () => {
     try {

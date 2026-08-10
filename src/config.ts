e0d59d94298102b5e3f564375d48f84582f05808
nikvn89 <nikkyvn89@gmail.com>
2026-08-10 23:34:36 +0700

    Delete src directory

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-export const CONTRACT_ADDRESS =
-  (import.meta.env.VITE_CONTRACT_ADDRESS as `0x${string}` | undefined) ??
-  '0x3a72Af40212f9075F13495Aa27cD58C6F4127337'
-
-export const STUDIO_RPC =
-  (import.meta.env.VITE_STUDIO_RPC as string | undefined) ??
-  'https://studio.genlayer.com/api'
